fd9904f4f30c782d3bfe7c57adb4df2ff27bcf4e
dcyao <1207542617@qq.com>
2023-07-19 16:50:31 +0800

    增加了three.js的代码

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,38 +1,59 @@
+<template>
+ <div ref="container" id="container" class="container"></div>
+</template>
 <script setup lang="ts">
-import { ref } from "vue";
+import { onMounted, ref } from "vue";
+import {
+  Color,
+  PerspectiveCamera,
+  Scene,
+  Mesh,
+  BoxGeometry,
+  WebGLRenderer,
+MeshBasicMaterial
+} from 'three'
 
+const container = ref()
+const scene = new Scene();
+scene.background = new Color('skyblue')
+
+
+onMounted(() => {
+  console.log('--container', container.value)
+  const fov = 35;
+  const dom: HTMLElement = container.value as HTMLElement
+  // const dom= document.getElementById("container");
+  const aspect = dom?.clientWidth / dom?.clientHeight
+  const near = 0.1;
+  const far = 100;
+
+  const camera= new PerspectiveCamera(
+    fov,aspect, near, far 
+  )
+
+  const geometry = new BoxGeometry(2,2,2)
+
+  const material = new MeshBasicMaterial();
+  const cube = new Mesh(geometry, material);
+  scene.add(cube);
+  const renderer = new WebGLRenderer();
+  dom.append(renderer.domElement)
+  // document.append(renderer.domElement)
+  renderer.setSize(dom.clientWidth, dom.clientHeight);
+  renderer.setPixelRatio(window.devicePixelRatio)
+  renderer.render(scene, camera)
+
+})
 defineProps<{ msg: string }>();
 
 const count = ref(0);
 </script>
 
-<template>
-  <h1>{{ msg }}</h1>
 
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
-</template>
 
 <style scoped>
-.read-the-docs {
-  color: #888;
+.container {
+  width: 200px;
+  height: 200px;
 }
 </style>
